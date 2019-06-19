34c391987b747d51c4f983df73c3385abe528bb7
Ziqian Ge <ge.zi@husky.neu.edu>
2019-06-19 14:47:37 -0400

    Restaurant model exported

--- models/RestaurantModel.js
+++ models/RestaurantModel.js
@@ -2,3 +2,5 @@ const mongoose = require('mongoose');
 const RestaurantSchema = require('./schemata/RestaurantSchema');
 
 const RestaurantModel = mongoose.model('RestaurantModel', RestaurantSchema);
+
+module.exports = RestaurantModel;

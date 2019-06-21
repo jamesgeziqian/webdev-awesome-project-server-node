067faa31f7c6b6f3cde559d164470d30005a6960
Ziqian Ge <ge.zi@husky.neu.edu>
2019-06-21 18:21:15 -0400

    password filtered

--- daos/UserDao.js
+++ daos/UserDao.js
@@ -2,9 +2,9 @@ const UserModel = require('../models/UserModel');
 
 const createUser = (user) => UserModel.UserModel.create(user);
 
-const findAllUsers = () => UserModel.UserModel.find();
+const findAllUsers = () => UserModel.UserModel.find({}, {"password": 0});
 
-const findUserById = (userId) => UserModel.UserModel.findById(userId);
+const findUserById = (userId) => UserModel.UserModel.findById(userId, {"password": 0});
 
 const findUser = (condition) => UserModel.UserModel.findOne(condition);
 

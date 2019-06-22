ca8e9a2d7bf0a5844d32455e649e380b8b080877
Ziqian Ge <ge.zi@husky.neu.edu>
2019-06-22 17:37:47 -0400

    mongodb uri fixed

--- models/dbs.js
+++ models/dbs.js
@@ -1,10 +1,8 @@
 const db = require('mongoose');
 
-// let mongodbURI = "mongodb://localhost:27017";
-// if (process.env.MONGODB_URI) {
-//     mongodbURI = process.env.MONGODB_URI;
-// }
-
-mongodbURI = "mongodb://heroku_1429tlr3:a5e5mu5ffe8b1her4oa6klmfpo@ds341837.mlab.com:41837/heroku_1429tlr3/webdev-awesome-project";
+let mongodbURI = "mongodb://localhost:27017/webdev-awesome-project";
+if (process.env.MONGODB_URI) {
+    mongodbURI = process.env.MONGODB_URI;
+}
 
 module.exports = db.connect(mongodbURI, {useNewUrlParser: true});

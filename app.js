883c475f1ef7f0af695fc42fd6859112a55a5945
Ziqian Ge <ge.zi@husky.neu.edu>
2019-06-22 20:51:08 -0400

    cors trusted origins added

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(session({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: "http://localhost:3001",
+  origin: ["http://localhost:3001", "https://webdev-awesome-project.herokuapp.com"],
   credentials: true
 }));
 
